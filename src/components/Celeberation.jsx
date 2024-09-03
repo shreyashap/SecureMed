cc24ed8fa72d7f5d1244ce998ff5876c9233af2d
shreyashap <107754587+shreyashap@users.noreply.github.com>
2024-09-04 00:02:04 +0530

    Coffecti changes

--- src/components/Celeberation.jsx
+++ src/components/Celeberation.jsx
@@ -1,7 +1,13 @@
 import Confetti from "react-confetti";
 
 const Celeberation = () => {
-  return <Confetti width={window.innerWidth} height={window.innerHeight} />;
+  return (
+    <Confetti
+      width={window.innerWidth}
+      height={window.innerHeight}
+      gravity={0.2}
+    />
+  );
 };
 
 export default Celeberation;
